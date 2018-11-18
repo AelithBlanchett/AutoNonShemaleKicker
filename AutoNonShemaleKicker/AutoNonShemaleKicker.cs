2d08fce2837ab96b2d08da3fe79c92fa2cfda588
Aelith Blanchett <tonyteur@yahoo.fr>
2018-11-18 15:47:08 -0500

    added notification system on new character

--- AutoNonShemaleKicker/AutoNonShemaleKicker.cs
+++ AutoNonShemaleKicker/AutoNonShemaleKicker.cs
@@ -1,6 +1,7 @@
 ﻿using FChatSharpLib.Entities.Plugin;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading;
 
@@ -9,21 +10,31 @@ namespace AutoNonShemaleKicker
     public class AutoNonShemaleKicker : BasePlugin
     {
         private Timer _kickMonitor;
+        private List<string> subscribedCharacters = new List<string>();
+        private bool isUsingDefaultStatus = false;
 
         public AutoNonShemaleKicker(string channel) : base(nameof(AutoNonShemaleKicker), "1.0.0", channel)
         {
-            _kickMonitor = new Timer(AutoKickNonShemales, null, 1000, 10000);
+            _kickMonitor = new Timer(PeriodicallyCheckThings, null, 1000, 5000);
             base.Run();
         }
 
-        private void AutoKickNonShemales(object state)
+        private void PeriodicallyCheckThings(object state)
         {
             if (!FChatClient.IsUserAdmin(FChatClient.State.BotCharacterName, Channel))
             {
                 return;
             }
             var characters = FChatClient.State.GetAllCharactersInChannel(Channel);
-            var subscribedCharacters = SDSWQueryAPI.GetAllCharacters();
+            var newListSubscribedCharacters = SDSWQueryAPI.GetAllCharacters();
+            var newCharacters = new List<string>();
+            if(subscribedCharacters.Count != 0)
+            {
+                newCharacters = newListSubscribedCharacters.Except(subscribedCharacters).ToList();
+            }
+
+            subscribedCharacters = newListSubscribedCharacters;
+
             foreach (var character in characters)
             {
                 if ((character.Gender != FChatSharpLib.Entities.Events.Helpers.GenderEnum.Shemale || (subscribedCharacters.Count > 0 && !subscribedCharacters.Contains(character.Character))) && !FChatClient.IsSelf(character.Character) && !FChatClient.IsUserAdmin(character.Character, Channel))
@@ -31,6 +42,18 @@ namespace AutoNonShemaleKicker
                     FChatClient.KickUser(character.Character, Channel);
                     FChatClient.SendPrivateMessage("Hello! You've been kicked out from the Shemale DS World ([session=Shemale DS World]adh-ae6c36cf75c40ec6a52a[/session]) channel because you're not an active player of that game.\nIf you'd like to register, please join the OOC room ([session=Shemale DS Game]adh-3c6c8a69c06c14bbf523[/session]) and read the description.\nThank you!", character.Character);
                 }
+            }
+
+            if (newCharacters.Count == 0 && !isUsingDefaultStatus)
+            {
+                FChatClient.SetStatus(FChatSharpLib.Entities.Events.Helpers.StatusEnum.Looking, $"[color=pink]Come join other bitch-breakers @ [session=Shemale DS Game]adh-3c6c8a69c06c14bbf523[/session]. Read the room's description for instructions on how to join the game.[/color]");
+                isUsingDefaultStatus = true;
+            }
+
+            foreach (var character in newCharacters)
+            {
+                FChatClient.SetStatus(FChatSharpLib.Entities.Events.Helpers.StatusEnum.Looking, $"[icon]{character}[/icon] [user]{character}[/user] [color=pink]just joined the game![/color]");
+                isUsingDefaultStatus = false;
             }
         }
     }
--- AutoNonShemaleKicker/Program.cs
+++ AutoNonShemaleKicker/Program.cs
@@ -7,6 +7,7 @@ namespace AutoNonShemaleKicker
         static void Main(string[] args)
         {
             var channelToWatch = "adh-ae6c36cf75c40ec6a52a";
+            channelToWatch = "adh-7546fab4ae1b849898da";
             var plugin = new AutoNonShemaleKicker(channelToWatch);
         }
     }
